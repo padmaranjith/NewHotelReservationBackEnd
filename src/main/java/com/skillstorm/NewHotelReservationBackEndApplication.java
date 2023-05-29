c3af858a6303c0c2470ff3d9a8a99cc3d54f2cc3
padma <padmaranjith28@gmail.com>
2023-05-29 07:50:27 -0400

    added spring project

---
+++ src/main/java/com/skillstorm/NewHotelReservationBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewHotelReservationBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewHotelReservationBackEndApplication.class, args);
+	}
+
+}
